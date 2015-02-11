f193b0c26ab9eac642290f996a4410a95f8c079e
Hernan Grecco <hernan.grecco@gmail.com>
2015-02-10 21:01:18 -0300

    Added stub for events (required for GPIB)

--- pyvisa-sim/highlevel.py
+++ pyvisa-sim/highlevel.py
@@ -277,3 +277,10 @@ class SimVisaLibrary(highlevel.VisaLibraryBase):
 
         return sess.set_attribute(attribute, attribute_state)
 
+    def disable_event(self, session, event_type, mechanism):
+        # TODO: implement this for GPIB finalization
+        pass
+
+    def discard_events(self, session, event_type, mechanism):
+        # TODO: implement this for GPIB finalization
+        pass
