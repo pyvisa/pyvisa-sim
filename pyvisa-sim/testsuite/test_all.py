9f0045eab6e1f91f135c8b1ded6c55b248d0d222
Huan Nguyen <hnguyen02@intersil.com>
2015-03-04 01:09:09 -0600

    Testsuite update to add coverage for new features

--- pyvisa-sim/testsuite/test_all.py
+++ pyvisa-sim/testsuite/test_all.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from pyvisa.testsuite import BaseTestCase
+from pyvisa.errors import VisaIOError
 import visa
 
 
@@ -13,12 +14,43 @@ class TestAll(BaseTestCase):
     def test_list(self):
         self.assertEqual(set(self.rm.list_resources()),
                          set(('GPIB0::8::65535::INSTR',
+                              'GPIB0::9::65535::INSTR',
                               'TCPIP0::localhost::inst0::INSTR',
                               'ASRL1::INSTR',
                               'USB0::0x1111::0x2222::0x1234::0::INSTR')))
     def test_devices(self):
-        for rn in self.rm.list_resources():
+        run_list = (
+            'GPIB0::8::65535::INSTR',
+            'TCPIP0::localhost::inst0::INSTR',
+            'ASRL1::INSTR',
+            'USB0::0x1111::0x2222::0x1234::0::INSTR'
+            )
+        for rn in run_list:
             self._test_device(rn)
+    
+    def test_device_2(self):
+        resource_name = 'GPIB0::9::65535::INSTR'
+        inst = self.rm.open_resource(
+            resource_name,
+            read_termination='\n',
+            write_termination='\r\n' if resource_name.startswith('ASRL') else '\n'
+            )
+        inst.write('FAKE_COMMAND')
+        status_reg = inst.query('*ESR?')
+        self.assertEqual(
+            int(status_reg),
+            32,
+            'invalid command test'
+            )
+        with self.assertRaises(VisaIOError):
+            inst.write(':VOLT:IMM:AMPL 2.00')
+            inst.read()
+        status_reg = inst.query('*ESR?')
+        self.assertEqual(
+            int(status_reg),
+            4,
+            'unexpected read test'
+            )
 
     def _test(self, inst, a, b):
         self.assertEqual(inst.query(a), b, msg=inst.resource_name + ', %r == %r' % (a, b))
