ad7006ad769504030ec67d274fa9c93df31f22de
Douglas Thor <doug.thor@gmail.com>
2023-03-07 20:52:41 -0800

    Move common session USB code to a BaseUSBSession class

--- pyvisa_sim/sessions/usb.py
+++ pyvisa_sim/sessions/usb.py
@@ -5,14 +5,15 @@
 :license: MIT, see LICENSE for more details.
 
 """
+from typing import Union
+
 from pyvisa import constants, rname
 
 from . import session
 
 
-@session.Session.register(constants.InterfaceType.usb, "INSTR")
-class USBInstrumentSession(session.Session):
-    parsed: rname.USBInstr
+class BaseUSBSession(session.Session):
+    parsed: Union[rname.USBInstr, rname.USBRaw]
 
     def after_parsing(self) -> None:
         self.attrs[constants.ResourceAttribute.interface_number] = int(
@@ -28,24 +29,13 @@ class USBInstrumentSession(session.Session):
         self.attrs[constants.ResourceAttribute.usb_interface_number] = int(
             self.parsed.board
         )
+
+
+@session.Session.register(constants.InterfaceType.usb, "INSTR")
+class USBInstrumentSession(BaseUSBSession):
+    parsed: rname.USBInstr
 
 
 @session.Session.register(constants.InterfaceType.usb, "RAW")
-class USBRawSession(session.Session):
-
+class USBRawSession(BaseUSBSession):
     parsed: rname.USBRaw
-
-    def after_parsing(self) -> None:
-        self.attrs[constants.ResourceAttribute.interface_number] = int(
-            self.parsed.board
-        )
-        self.attrs[
-            constants.ResourceAttribute.manufacturer_id
-        ] = self.parsed.manufacturer_id
-        self.attrs[constants.ResourceAttribute.model_code] = self.parsed.model_code
-        self.attrs[
-            constants.ResourceAttribute.usb_serial_number
-        ] = self.parsed.serial_number
-        self.attrs[constants.ResourceAttribute.usb_interface_number] = int(
-            self.parsed.board
-        )
