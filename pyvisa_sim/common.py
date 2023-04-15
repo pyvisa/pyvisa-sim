8d0fdcfb1ec5bc25f594e8d4333160e732104bef
Douglas Thor <doug.thor@gmail.com>
2023-04-15 15:00:27 -0700

    Fix iter_bytes implementation

--- pyvisa_sim/common.py
+++ pyvisa_sim/common.py
@@ -9,29 +9,51 @@ Do not edit here.
 
 """
 import logging
-from typing import Callable, Optional, Sequence
+from typing import Callable, Iterator, Optional, Sequence
 
 from pyvisa import logger
 
 logger = logging.LoggerAdapter(logger, {"backend": "sim"})  # type: ignore
 
 
-def iter_bytes(data: bytes, mask: Optional[int] = None, send_end: bool = False):
-    if send_end and mask is None:
-        raise ValueError("send_end requires a valid mask.")
+def iter_bytes(
+    data: bytes, data_bits: Optional[int] = None, send_end: bool = False
+) -> Iterator[bytes]:
+    """Clip values to the correct number of bits per byte.
 
-    if mask is None:
+    Serial communication may use from 5 to 8 bits.
+
+    Parameters
+    ----------
+    data : The data to clip as a byte string.
+    data_bits : How many bits per byte should be sent. Clip to this many bits.
+        For example: data_bits=5: 0xff (0b1111_1111) --> 0x1f (0b0001_1111)
+    send_end : If True, send the final byte unclipped (with all 8 bits).
+
+    References
+    ----------
+    + https://www.ivifoundation.org/downloads/Architecture%20Specifications/vpp43_2022-05-19.pdf,
+    + https://www.ni.com/docs/en-US/bundle/ni-visa/page/ni-visa/vi_attr_asrl_data_bits.html,
+    + https://www.ni.com/docs/en-US/bundle/ni-visa/page/ni-visa/vi_attr_asrl_end_out.html
+    """
+    if send_end and data_bits is None:
+        raise ValueError("'send_end' requires a valid 'data_bits' value.")
+
+    if data_bits is None:
         for d in data:
             yield bytes([d])
-
     else:
+        # 2**8     = 0b1000_0000
+        # 2**8 - 1 = 0b0111_1111
+        mask = 2**data_bits - 1
+
         for d in data[:-1]:
-            yield bytes([d & ~mask])
+            yield bytes([d & mask])
 
         if send_end:
-            yield bytes([data[-1] | ~mask])
+            yield bytes([data[-1]])
         else:
-            yield bytes([data[-1] & ~mask])
+            yield bytes([data[-1] & mask])
 
 
 int_to_byte: Callable[[int], bytes] = lambda val: bytes([val])
--- pyvisa_sim/testsuite/test_common.py
+++ pyvisa_sim/testsuite/test_common.py
@@ -6,35 +6,34 @@ from pyvisa_sim import common
 
 
 @pytest.mark.parametrize(
-    "data, mask, send_end, want",
+    "data, data_bits, send_end, want",
     [
-        (b"1234", None, False, b"1234"),
-        (b"41234", 3, False, b"40004"),
-        # Can't figure this one out. Getting ValueError: bytes must in in range(0, 256)
-        # If I knew more about the purpose of `iter_bytes` then maybe I could
-        # reconcile it, but I don't have time to investigate right now.
-        pytest.param(
-            b"1234",
-            3,
-            True,
-            b"1234",
-            marks=pytest.mark.xfail(
-                reason=(
-                    "ValueError bytes must be in range(0, 256). TODO: figure"
-                    " out correct 'want' value."
-                )
-            ),
-        ),
+        (b"\x01", None, False, b"\x01"),
+        (b"hello world!", None, False, b"hello world!"),
+        # Ensure clipping works as expected
+        (b"\x04", 2, False, b"\x00"),  # 0b0100 --> 0b0000
+        (b"\x04", 2, True, b"\x04"),  # 0b0100 --> 0b0100
+        (b"\x05", 2, False, b"\x01"),  # 0b0101 --> 0b0001
+        (b"\xff", 7, False, b"\x7f"),  # 0b1111_1111 --> 0b0111_1111
+        (b"\xff", 7, True, b"\xff"),  # 0b1111_1111 --> 0b1111_1111
+        # Clipping with 8 or more bits does nothing, as `data` is bytes
+        # which is limited to 8 bits per character.
+        (b"\xff", 8, False, b"\xff"),
+        (b"\xff", 9, False, b"\xff"),
+        # Make sure we're iterating correctly
+        (b"\x6d\x5c\x25", 4, False, b"\r\x0c\x05"),
+        (b"\x6d\x5c\x25", 4, True, b"\r\x0c\x25"),
+        (b"`\xa0", 6, False, b"  "),
     ],
 )
 def test_iter_bytes(
-    data: bytes, mask: Optional[int], send_end: bool, want: List[bytes]
+    data: bytes, data_bits: Optional[int], send_end: bool, want: List[bytes]
 ) -> None:
-    got = b"".join(common.iter_bytes(data, mask=mask, send_end=send_end))
+    got = b"".join(common.iter_bytes(data, data_bits=data_bits, send_end=send_end))
     assert got == want
 
 
-def test_iter_bytes_with_send_end_requires_mask() -> None:
+def test_iter_bytes_with_send_end_requires_data_bits() -> None:
     with pytest.raises(ValueError):
         # Need to wrap in list otherwise the iterator is never called.
-        list(common.iter_bytes(b"", mask=None, send_end=True))
+        list(common.iter_bytes(b"", data_bits=None, send_end=True))
